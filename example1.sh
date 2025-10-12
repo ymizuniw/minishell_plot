5faa8584b58aa89257d1227be00d3d904bf4d7c4
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-12 18:10:23 +0900

    renew

--- example1.sh
+++ example1.sh
@@ -6,9 +6,11 @@ a=1
 set 2 
 echo ${a}b-$ab-${1}0-${10}-$10 
 
-# echo "
-# "
+echo "
+"
 
-# echo "--test2--
-# "
+echo "--test2--
+"
 
+#if ${var-*} comes, var will be expanded.
+#"foo=" means put null to foo.
