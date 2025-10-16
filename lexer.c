2fd013f56ff8f874db2ba1a02d5ed93a82e6f966
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-16 14:00:41 +0900

    parser analisis

--- lexer.c
+++ lexer.c
@@ -1,110 +1,118 @@
 #include "minishell.h"
 
-// Breaks the input into words and operators,
-// 	obeying the quoting rules described in Quoting.
-// 	These tokens are separated by metacharacters.
-// This step performs alias expansion(see Aliases)
-
-t_token	*lexer(const char *);
-
-get_word(char **word, char *input, size_t input_len, size_t *idx)
+int append_token(t_token *head, t_token *new)
 {
-    char quote;
-    char *quote_open=NULL;
-    char *quote_close=NULL;
-    size_t tmp_idx=*idx;
-    size_t word_len=strlen(word);
-    size_t consumed_idx=0;
-    bool word_end=0;
-
-    quote = is_quote(input[*idx]);
-    if (quote != 0)
+    size_t i=0;
+    t_token *cur;
+    if (head==NULL)
+        return (0);
+    cur = head->next;
+    while (i<head->size)
     {
-        quote_open = &input[tmp_idx];
-        //1."fuck fuck"
-        //2."fuck" fuck
-        //3.""
-        //4."fuck"fuck
-        tmp_idx = idx + 1;
-        quote_close = strchr(quote_open, quote);
-        if (quote_close==NULL)
-        {
-            //unquoted err.
-        }
-        if (quote_close[1]=='\0' || get_meta_char(quote_close[1])!=MT_OTHER)
-            word_end = 1;
-        size_t new_len = quote_close - &input[tmp_idx];
-        if (new_len>=2)
-        {
-            char *word = realloc(word_len + new_len, sizeof(char));
-            if (word==NULL)
-            {
-                //calloc err.
-            }
-            strlcpy(word, &input[tmp_idx], word_len);
-            word[word_len]='\0';
-        }
-        consumed_idx = word_len+1;
+        cur=cur->next;
     }
-    else
-    {
-        word_end = true;
-        size_t word_len = 0;
-        while (tmp_idx < input_len && get_meta_char(input[tmp_idx])!=MT_OTHER)
-        {
-            tmp_idx++;
-            word_len++;
-        }
-        char *word = calloc(word_len, sizeof(char));
-        if (word==NULL)
-        {
-            //calloc err.
-        }
-        strlcpy(word, &input[tmp_idx], word_len);
-        word[word_len]='\0';
-        consumed_idx = word_len-1;
-    }
-    *idx += consumed_idx;
-    if (word_end==false)
-        get_word(word, input, input_len, *idx);
+    cur->next = new;
+    head->size++;
+    return (1);
 }
 
-t_token	*lexer(const char *input)
+//if the token type is TK_WORD, then word_concatenation() concatenate words 
+//1. WORD * WORD (tokenization occurs first.)
+//2. WORD (cocatenate before tokenization)
+      size_t word_concatenation(char **word, size_t word_len, char *input,size_t input_len, size_t idx)
+        {
+            size_t consumed_len=0;
+            //if quote comes
+            unsigned char quote_open = is_quote(input[idx]);
+            if (quote_open)
+            {
+                char *quote_close = strchr(&input[idx+1], quote_open);
+                if (!quote_close)
+                {
+                    fprintf(stderr, "syntax error: unclosed quote\n");
+                    return NULL;
+                }
+                //extract word between the quotation.
+                size_t new_len = quote_close - &input[idx];
+                *word = realloc(word_len + new_len + 1, sizeof(char));
+                if (!word)
+                    return NULL;
+                strlcpy(*word + word_len, &input[idx], new_len + 1);
+                consumed_len = word_len + 2;
+            }
+            //quotation does not come.
+            else
+            {
+                char *tmp_ptr = &input[idx];
+                while (idx < input_len && !isspace((unsigned char)input[idx])
+                    && is_metachar(input[idx]) == MT_OTHER)
+                        tmp_ptr++;
+                size_t new_len = tmp_ptr - &input[idx];
+                *word = realloc(word_len + 1, sizeof(char));
+                if (!*word)
+                    return NULL;
+                strlcpy(*word + word_len, &input[idx], new_len + 1);
+                consumed_len = new_len;
+            }
+            idx += consumed_len;
+            if (idx<input_len && is_meta_char(input[idx])==MT_OTHER)
+                idx = word_cocatenation(word, word_len, input, input_len, idx);
+            return (idx);
+        }
+
+t_token *lexer(const char *input)
 {
-	t_metachar	meta;
-	t_token		*token_head;
-    t_token     *cur=NULL;
-	size_t		idx;
-	size_t		input_len;
-	char		quote;
-	size_t		quote_open_idx;
-	size_t		quote_close_idx;
-	size_t		tmp_idx;
-    size_t consumed_idx;
+    size_t idx = 0;
+    size_t tmp_idx = 0;
+    size_t input_len = strlen(input);
+    size_t consumed_idx = 0;
+    t_token *token_head;
+    char **word=NULL;
 
     token_head = token_alloc();
+    if (token_head==NULL)
+    {
+        perror("token_alloc");
+        exit(1);
+    }
     bzero(token_head, sizeof(t_token));
-    cur=token_head;
-	input_len = strlen(input);
-	idx = 0;
-	while (idx < input_len)
-	{
-		meta = get_meta_char();
-		if (meta != MT_OTHER)
-		{
-			// here is to split idx.
-		}
-		else
-		{
-            char **word=NULL;
-            get_word(word, input, input_len, &idx);
-            t_token *new_token = gen_token();
-            bzero(new_token, sizeof(t_token));
-            new_token->type=TK_WORD;
-            set_quote_flag(new_token, quote_open);
-            new_token->value=*word;
-            idx += consumed_idx;
+    while (idx < input_len)
+    {
+        if (isspace((unsigned char)input[idx]))
+        {
+            idx++;
+            continue;
+        }
+        t_metachar meta = get_meta_char(input[idx]);
+        if (meta != MT_OTHER)
+        {
+            t_token *new = token_alloc();
+            new->type = get_token_type((char *)input, &idx);
+            set_token_value(new);
+            append_token(token_head, new);
+            idx++;
+            continue;
+        }
+        else
+        {
+            size_t word_len=0;
+            if (word && *word)
+                word_len = strlen(*word);
+            idx = word_concatenation(word, word_len, input, input_len, idx);
+            t_token *new = token_alloc();
+            new->type = TK_WORD;
+            size_t word_len = strlen(*word);
+            new->value = malloc(sizeof(char)*(word_len+1));
+            if (new->value==NULL)
+            {
+                perror("malloc");
+                exit(1);
+            }
+            new->value[word_len]='\0';
+            strlcpy(new->value, *word, word_len + 1);
+            new->value = *word;
+            new->next=NULL;
+            append_token(token_head, new);
         }
     }
-}
-// quote removal test case:
+}    
--- parser.c
+++ parser.c
@@ -2,6 +2,19 @@
 
 t_ast	*parser(t_token *);
 
-t_ast	*parser(t_token *token)
+t_ast	*parser(t_token *token_head)
 {
+    t_token *token = token_head->next;
+    //logic and pipe
+    
 }
+
+//consider the rule to generation grammar.
+
+1. <cmd1> | <cmd2>
+2. <cmd1> && <cmd2>
+3. <cmd1> && (cmd2 && cmd3)
+4. < <cmd1> 
+5. << <cmd1>
+6. <cmd1> > file_name
+7. <cmd1> >> file_name
