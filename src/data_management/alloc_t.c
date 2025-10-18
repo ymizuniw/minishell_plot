de9ed7cc1e7357996425523eba816eb75b13f820
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-19 02:16:34 +0900

    data_management updated

--- src/data_management/alloc_t.c
+++ src/data_management/alloc_t.c
@@ -1,8 +1,22 @@
 #include "../includes/minishell.h"
 
+t_ast *alloc_node(void)
+{
+    return (t_ast *)malloc(sizeof(t_ast));
+}
+
+t_cmd *alloc_cmd(void)
+{
+    return (t_cmd *)malloc(sizeof(t_ast));
+}
+
+t_token *alloc_token(void)
+{
+    return ((t_token *)malloc(sizeof(t_token)));
+}
+
 void *alloc_t(int type, size_t n)
 {
-    if (type==TYPE_AST)
         return ((t_ast *)malloc(sizeof(t_ast)*n));
     if (type==TYPE_TOKEN)
         return ((t_token *)malloc(sizeof(t_token)*n));
--- src/data_management/free_t.c
+++ src/data_management/free_t.c
@@ -1,21 +1,63 @@
 #include "../includes/minishell.h"
 
-void free_ast_tree()
+void free_ast_tree(t_ast *p)
 {
-
+    t_ast *cur = p;
+    if (cur->left!=NULL)
+        free_ast_tree(cur->left);
+    if (cur->right!=NULL)
+        free_ast_tree(cur->right);
+    if (p->cmd!=NULL)
+        free_cmd_structure(p->cmd);
+    if (p->subtree!=NULL)
+        free_subshell_tree(p->subtree);
+    free(p);
 }
 
-void free_subshell_tree()
+void free_subshell_tree(t_ast *p)
 {
-
+    free_ast_tree(p);
 }
 
-void free_token_list()
+void free_token_list(t_token *p)
 {
-
+    if (p->next!=NULL)
+        free_token_list(p->next);
+    if (p->type!=TK_WORD)
+        free(p->value);
+    free(p);
 }
 
-void free_cmd_structure()
+void free_double_array_contents(char **p, char *p_content)
 {
+    if (*p!=NULL)
+        free_double_array_contents(p, *p++);
+    free(*p);
+}
 
+void free_double_array(char **p)
+{
+    if (*p!=NULL)
+        free_double_array_contents(p, *p++);
+    free(p);
+}
+
+void free_redir_list(t_redir *p)
+{
+    if (p->next!=NULL)
+        free_redir_list(p->next);
+    if (p->filename!=NULL)
+        free(p->filename);
+    free(p);
+}
+
+void free_cmd_structure(t_cmd *p)
+{
+    if (p->argv!=NULL)
+        free_double_array(p->argv);
+    if (p->redir_in!=NULL)
+        free_redir_list(p->redir_in);
+    if (p->redir_out!=NULL)
+        free_redir_list(p->redir_out);
+    free(p);
 }
