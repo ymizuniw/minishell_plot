afe0d090212598115853d5f8b4e19f7ac53b625c
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2025-10-20 19:14:04 +0900

    ref

--- documents/heredoc_management/new_name_generator.c
+++ documents/heredoc_management/new_name_generator.c
@@ -1,6 +1,35 @@
 
 
 
+
+//plot1
+
+#define unique_set "fdas4312ghkj5678"
+
+char *gen_unique_name()
+{
+    char *name=NULL;
+    int  rand=0;
+    name=getenv("$PPID");
+    rand=atoi(name);
+
+    int mod=0;
+
+    size_t i=0;
+    char *tmp = malloc(sizeof(char)*(PATH_MAX+1));
+    while (1)
+    {
+        //cpy mod idx from unique set to tmp,
+
+        //check if it already exists
+            rand -= 3;
+            continue ;
+        //if not exit
+            return(tmp);
+    }
+}
+
+//plot2
 char *unique_filename(char *filename, size_t len, int *charset[], size_t times)
 {
     char c;
