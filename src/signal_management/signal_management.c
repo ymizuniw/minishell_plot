f283c256c455cb75e65824fcb217193c8c5a0a1f
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-26 14:48:21 +0900

    signal_management updated. ensurring that signal_handler are user defined when in parent process, or default in child process.

--- src/signal_management/signal_management.c
+++ src/signal_management/signal_management.c
@@ -1,30 +1,54 @@
 #include "../../includes/minishell.h"
 
-volatile sig_atomic_t g_recept_signal = 0;
+volatile sig_atomic_t g_signum = 0;
 
 static void signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		g_recept_signal = SIGINT;
-		write(STDOUT_FILENO, "\n", 1);
+		g_signum = SIGINT;
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();		
 	}
-	else return ;
 }
 
-int	signal_initializer(sig_atomic_t g_recept_signal)
+//just before execve, rest it default sigaction.
+int set_sig_dfl(void)
 {
 	struct sigaction sact;
 	sigemptyset(&sact.sa_mask);
 	sact.sa_flags = 0;
-	sact.sa_handler = SIG_IGN;
-	
-	
+	sact.sa_handler = SIG_DFL;
+	sigaction(SIGTERM, &sact NULL);
+	sigaction(SIGQUIT, &sact NULL);
+	sigaction(SIGINT, &sact NULL);
+	return (0);
 }
 
+int	signal_initializer(bool interactive)
+{
+	struct sigaction sact;
+	
+	if (interactive==true)
+	{
+		sigemptyset(&sact.sa_mask);
+		sact.sa_flags = 0;
+		sact.sa_handler = SIG_IGN;
+		sigaction(SIGTERM, &sact, NULL);
+		sigaction(SIGQUIT, &sact, NULL);
+
+		sact.sa_flags = 0;
+		sact.sa_handler = signal_handler;
+		sigaction(SIGINT, &sact, NULL);
+		return (0);
+	}
+	set_sig_dfl();
+	return(0);
+}
+
+//when bash is in interactive mode, SIGTERM are ignored. SIGINT is handled, and SIGQUIT is ignored in all cases.
+//if asyncronous command is under execution, SIGTERM and SIGINT are also ignored.
 int handle_child(int *last_exit_status, pid_t pid)
 {
 	int local_status = 0;
@@ -34,13 +58,13 @@ int handle_child(int *last_exit_status, pid_t pid)
 		*last_exit_status = 1;
 		return -1;
 	}
-	if (WIFEXITED(local_status))
-		*last_exit_status = WEXITSTATUS(local_status);
-	else if (WIFSIGNALED(local_status))
+	if (WIFSIGNALED(local_status))
 	{
-		*last_exit_status  = 128 + WTERMSIG(local_status);
+		*last_exit_status = WEXITSTATUS(local_status);
 		if (WTERMSIG(local_status)==SIGQUIT)
-			write(2, "Quit (core dumped)\n", 19);
+			write(STDERR_FILENO, "Quit (core dumped)\n", 19);
+		else if (WTERMSIG(local_status) == SIGINT)
+			write(STDOUT_FILENO, "\n", 1);
 	}
 	return 0;
 }
