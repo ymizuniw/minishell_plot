06b23c98e1b46127c66e0adc74ea9cdb34d5f255
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-28 02:04:24 +0900

    lexer devided for norm.

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,96 +1,175 @@
 #include "../../includes/minishell.h"
 
+int init_token(t_token **token_head)
+{
+	*token_head = alloc_token();
+	if (*token_head == NULL)
+	{
+		perror("token_alloc");
+		return (0);
+	}
+	memset(*token_head, 0, sizeof(t_token));
+	return (1);
+}
+
+int handle_newline(t_token *token_head, const char *input, size_t *idx)
+{
+	t_token		*new;
+
+	if (*idx>0)
+	{
+		char prev = input[*idx-1];
+		if (prev=='|' || prev == '&' || prev == '(')
+		{
+			*idx++;
+			return (1);
+		}
+	}
+	new = alloc_token();
+	if (!new)
+		return (-1);
+	memset(new, 0, sizeof(t_token));
+	new->type = TK_NEWLINE;
+	new->value = strdup("\n");
+	if (!new->value)
+		return (-1);
+	append_tokens(token_head, new);
+	*idx++;
+	return (1);
+}
+
+int handle_meta_char(t_token *token_head, const char *input, size_t *idx)
+{
+	t_token *new;
+	new = alloc_token();
+	if (!new)
+		return (-1);
+	memset(new, 0, sizeof(t_token));
+	new->type = get_token_type((char *)input, idx);
+	append_tokens(token_head, new);
+	return (1);
+}
+
+int handle_doller(t_token *token_head, char const *input, size_t *idx)
+{
+	t_token *new;
+	new = alloc_token();
+	if (!new)
+		return (-1);
+	memset(new, 0, sizeof(t_token));
+	new->type = TK_DOLLER;
+	new->value = strdup("$");
+	if (!new->value)
+		return (-1);
+	append_tokens(token_head, new);
+	*idx++;
+	return (1);
+}
+
+int handle_word(t_token *token_head, char const *input, size_t input_len, size_t *idx)
+{
+	t_token *new;
+	new = alloc_token();
+	if (!new)
+		return (-1);
+	memset(new, 0, sizeof(t_token));
+	
+	char	*word;
+	size_t consumed = 0;
+	word = NULL;
+	consumed = word_cat(&word, 0, (char *)input, input_len, *idx);
+	if (consumed == 0)
+	{
+		xfree(new);
+		free_token_list(token_head);
+		return (-1);
+	}
+	// if (is_quote(input[idx]))
+	// 	set_quote_flag(new, (char *)input, is_quote(input[idx]));
+	*idx += consumed;
+	new->type = TK_WORD;
+	new->value = word;
+	new->next = NULL;
+	append_tokens(token_head, new);	
+	return (1);
+}
+
+int handle_word_and_doller(t_token *token_head,char const *input,  size_t input_len, size_t *idx)
+{
+	if (is_doller_token(&input[*idx]))
+	{
+		if (handle_doller(token_head, input, idx)<0)
+			return (-1);
+	}
+	else
+	{
+		if (handle_word(token_head, input, input_len, idx)<0)
+			return (-1);
+	}
+	return (1);
+}
+
+int handle_eof(t_token *token_head)
+{
+	t_token *new = alloc_token();
+	if (!new)
+		return (-1);
+	new->type = TK_EOF;
+	if (!new->value)
+		new->value = strdup("");
+	new->next = NULL;
+	append_tokens(token_head, new);
+	return (1);
+}
+
+int handle_internal_separator(t_token *token_head, char const *input, size_t *idx)
+{
+	if (input[*idx]=='\n')
+	{
+		if (handle_newline(token_head, input, idx)<0)
+			return (-1);
+	}
+	if (input[*idx] && isspace((unsigned char)input[*idx]))
+		*idx++;
+	return (1);	
+}
+
+int handle_operators_and_words(t_token *token_head, char const *input, size_t input_len, size_t *idx)
+{
+	t_metachar	meta = is_meta_char(input[*idx]);
+	if (meta != MT_OTHER)
+	{
+		if (handle_meta_char(token_head, input, idx)<0)
+			return (-1);
+	}
+	else
+	{
+		if (handle_word_and_doller(token_head, input_len, input, idx)<0)
+			return (-1);
+	}
+	return (1);
+}
+
 t_token	*lexer(const char *input)
 {
 	size_t		idx;
 	size_t		input_len;
 	t_token		*token_head;
-	char		*word;
 	t_metachar	meta;
-	t_token		*new;
 	size_t		consumed;
 
 	idx = 0;
 	input_len = strlen(input);
-	token_head = alloc_token();
-	if (token_head == NULL)
-	{
-		perror("token_alloc");
-		exit(1);
-	}
-	memset(token_head, 0, sizeof(t_token));
 	while (idx < input_len)
 	{
-		if (input[idx]=='\n')
-		{
-			if (idx>0)
-			{
-				char prev = input[idx-1];
-				if (prev=='|' || prev == '&' || prev == '(')
-				{
-					idx++;
-					continue ;
-				}
-			}
-			new = alloc_token();
-			memset(new, 0, sizeof(t_token));
-			new->type = TK_NEWLINE;
-			new->value = strdup("\n");
-			append_tokens(token_head, new);
-			idx++;
-			continue ;
-		}
-		if (isspace((unsigned char)input[idx]))
-		{
-			idx++;
-			continue ;
-		}
-		new = alloc_token();
-		memset(new, 0, sizeof(t_token));
-		meta = is_meta_char(input[idx]);
-		if (meta != MT_OTHER)
-		{
-			new->type = get_token_type((char *)input, &idx);
-			append_tokens(token_head, new);
-			continue ;
-		}
-		else
-		{
-			if (is_doller_token(&input[idx]))
-			{
-				new->type = TK_DOLLER;
-				new->value = strdup("$");
-				append_tokens(token_head, new);
-				continue ;
-			}
-			else
-			{
-				word = NULL;
-				consumed = word_cat(&word, 0, (char *)input, input_len, idx);
-				if (consumed == 0)
-				{
-					xfree(new);
-					free_token_list(token_head);
-					return (NULL);
-				}
-				// if (is_quote(input[idx]))
-				// 	set_quote_flag(new, (char *)input, is_quote(input[idx]));
-				idx += consumed;
-				new->type = TK_WORD;
-				new->value = word;
-				new->next = NULL;
-				append_tokens(token_head, new);
-			}
-		}
+		if (handle_internal_separator(token_head, input, &idx)<0)
+			return (NULL);
+		if (idx >= input_len)
+			break ;
+		if (handle_operators_and_words(token_head, input, input_len, &idx)<0)
+			return (NULL);
 	}
-	new = alloc_token();
-	if (new)
-	{
-		new->type = TK_EOF;
-		if (!new->value)
-			new->value = strdup("");
-		new->next = NULL;
-		append_tokens(token_head, new);
-	}
+	if (handle_eof(token_head)<0)
+		return(NULL);
 	return (token_head);
 }
