1fe257325701a3fbbdd87be4dba6f4c668276bf0
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-10-28 15:06:59 +0900

    expand_value() in parser_utils is devided for norm.

--- src/parser/parser_utils/expand_value.c
+++ src/parser/parser_utils/expand_value.c
@@ -1,59 +1,80 @@
 #include "../../../includes/minishell.h"
 
-// just expand the value content or apply $ expansion and form a word.
-char	*expand_value(t_token *token)
+int handle_no_expansion(char **word, char const *value, size_t word_len_no_expansion)
 {
-	size_t	word_len;
-	char	*word;
-	char	*doller;
+	word = xmalloc(sizeof(char) * (word_len_no_expansion + 1));
+	if (!word)
+		return (-1);
+	strncpy(word, token->value, word_len_no_expansion + 1);
+	return (1);
+}
+
+int handle_doller_expansion(char const **word, char const *value, char *doller)
+{
 	size_t	i;
 	char	*var;
 	char	*entry;
 	size_t	start;
 
-	word_len = strlen(token->value);
-	word = NULL;
-	if (token->in_dquote || token->in_squote)
+	i=0;
+	while (doller && doller[i])
 	{
-		word = xmalloc(sizeof(char) * (word_len + 1));
-		if (!word)
-			return (NULL);
-		strncpy(word, token->value, word_len + 1);
+		start = i;//start point of searching.
+		i++;//next char.
+		while (doller[i] && !isspace(doller[i]) && doller[i] != '$')
+			i++;//if space or $ is following '$', it is the end of $ expansion.
+		var = xmalloc(sizeof(char)*(i-start+1));//malloc() the length of var to expand + null term.
+		if (!var)
+			return (-1);
+		memset(var, 0, sizeof(char)*(i-start+1));
+		strncpy(var, doller + 1, i - start);//copy after doller to var of the length of i - start.
+		entry = genenv(var);//search env var from env_list. this will be changed for local env list.
+		if (entry!=NULL)//if entry found,
+			cat_word(word, entry, strlen(entry));
+		xfree(var);
+		doller = strchr(doller + 1, '$');
+	}
+	return (1);
+}
+
+int search_doller_and_expand(char **word, char const *value, size_t word_len_no_expansion)
+{
+	char	*word;
+	char	*doller;
+
+
+	doller = strchr(value, '$');
+	if (doller != NULL)
+	{
+		if (handle_doller_expansion(word, value, doller)<0)
+			return (-1);
 	}
 	else
 	{
-		doller = strchr(token->value, '$');
-		if (doller != NULL)
-		{
-			i = 0;
-			while (doller && doller[i])
-			{
-				start = i;
-				i++;
-				while (doller[i] && !isspace(doller[i]) && doller[i] != '$')
-					i++;
-				var = xmalloc(sizeof(char) * (i - start + 1));
-				if (!var)
-				{
-					xfree(word);
-					return (NULL);
-				}
-				memset(var, 0, sizeof(char) * (i - start + 1));
-				strncpy(var, doller + start + 1, i - start);
-				entry = getenv(var);
-				if (entry != NULL)
-					cat_word(&word, entry, strlen(entry));
-				xfree(var);
-				if (doller[i] == '$')
-					doller = &doller[i];
-				else
-					doller = NULL;
-			}
-		}
-		else
-		{
-			word = strdup(token->value);
-		}
+		if (handle_no_expansion(word, value, word_len_no_expansion)<0)
+			return (-1);
+	}
+	return (1);
+}
+
+// just expand the value content or apply $ expansion and form a word.
+char	*expand_value(t_token *token)
+{
+	size_t	word_len_no_expansion;
+	char *word;
+
+	//initialize
+	word_len_no_expansion = strlen(token->value);
+	word = NULL;
+	if (token->in_squote || token->type == TK_DOLLER)
+	{
+		if (handle_no_expansion(&word, token->value, word_len_no_expansion)<0)
+			return (NULL);
+	}
+	else
+	{
+		if (search_doller_and_expand(&word, token->value, word_len_no_expansion)<0)
+			return (NULL);
 	}
 	return (word);
 }
