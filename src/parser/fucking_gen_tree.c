8816b33834dafb0760a6adec1a04abf4b43dece6
ymizuniw <newerdaysreki@gmail.com>
2025-10-30 02:38:08 +0900

    fucking_gen_tree.c is added. it is re-written for more compatibility.

--- src/parser/fucking_gen_tree.c
+++ src/parser/fucking_gen_tree.c
@@ -1,25 +1,5 @@
-#include "../../includes/minishell.h"
 
-t_ast	*parser(t_token *dummy_head)
-{
-	t_token	*token;
-	t_ast	*root;
-
-	if (!dummy_head)
-		return (NULL);
-	root = NULL;
-	token = dummy_head->next;
-	if (!token)
-		return (NULL);
-	root = gen_tree(NULL, &token, 0);
-	if (!root)
-	{
-		return (NULL);
-	}
-	while (token && token->type == TK_NEWLINE)
-		token = token->next;
-	return (root);
-}
+#include "../../../includes/minishell.h"
 
 int	token_is_redirection(t_token_type type)
 {
